eec00b05aec2f141320b05dfdf7c1ed12d18034c
bhagyainala <156509930+bhagyainala@users.noreply.github.com>
2024-01-13 17:30:24 +0530

    Recipes app with search functionality

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      We cannot get enough of these sheet-pan suppers: Simple preparations, complete meals cooked on one sheet for easy clean-ups, they save the day on busy weeknights. These top-rated sheet-pan recipes get the job done with chicken in the starring role, ably accompanied by the kind of tasty vegetables that chicken loves best: potatoes, broccoli, peppers, Brussels sprouts, cauliflower, green beans, and more.
+      </p>
+    </section>
+  )
+}
+
+export default About
